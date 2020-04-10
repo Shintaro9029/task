0819148282f45302f331b35252122eb13c4f06a5
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-04-10 14:21:48 +0900

    400,500のエラーページを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,23 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     before_action :login_required
 
+    #user_contorollerに継承させるために
     class Forbidden < ActionController::ActionControllerError
     end
 
-    rescue_from Forbidden, with: :rescue403
+    rescue_from Forbidden,                      with: :rescue403
+
+    rescue_from Exception,                      with: :rescue500
+    rescue_from ActiveRecord::RecordNotFound,   with: :rescue404
+    rescue_from ActionController::RoutingError, with: :rescue404
+
+    def routing_error
+        raise ActionController::RoutingError, params[:path]
+    end
+
+    
 
     private
-
     def current_user
         @current_user||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
@@ -19,5 +29,14 @@ class ApplicationController < ActionController::Base
 
     def rescue403
         render template: 'errors/forbidden', status: 403
+    end
+
+
+    def rescue404
+        render template: 'errors/not_found', status: 404
+    end
+
+    def rescue500
+        render template: 'errors/internal_sever_error', status: 500
     end
 end
