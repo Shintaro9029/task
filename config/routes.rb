4c67772b30eb7f5abf115d7085ca18cf90bb4724
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-04-25 04:38:46 +0900

    プレコンパイル修正 アクセス成功

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do  
-  namespace :admin do
-    resources :users
-  end
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  namespace :admin do
+    resources :users
+  end
   
   root to: 'tasks#index'
   resources :tasks
